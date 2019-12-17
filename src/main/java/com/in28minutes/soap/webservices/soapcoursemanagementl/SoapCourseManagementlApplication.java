bc45bfe8bfc4a3be89ea2d8078e8ca856f1b590d
THEODORIDIS Leonidas <leonidas.theodoridis@soprasteria.com>
2019-12-17 14:21:42 +0100

    Generation of the project from Spring Initializer

---
+++ src/main/java/com/in28minutes/soap/webservices/soapcoursemanagementl/SoapCourseManagementlApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.soap.webservices.soapcoursemanagementl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCourseManagementlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCourseManagementlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/soap/webservices/soapcoursemanagementl/SoapCourseManagementlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.soap.webservices.soapcoursemanagementl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagementlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
